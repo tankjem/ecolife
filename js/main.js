50c9b68d32648cc4b83d14756b8f89c4115a1227
Jeremy Smith <jmsmith57@hotmail.com>
2016-10-11 11:42:19 +0100

    added some more css and a thank you page after information request

--- js/main.js
+++ js/main.js
@@ -11,22 +11,10 @@ $(document).ready(function(){
 
   var enquiry = $('#newEnquiry');
   enquiry.on("submit", postThankYou);
-  // console.log("clicked");
 
   function postThankYou(e){
     e.preventDefault();
-    console.log("clicked");
+    window.location.href = "thanks.html";
   }
-  
+
 });
-// var newEnquiry = $('#enquiry');
-// newEnquiry.on("click", createThankYou);
-
-// function createThankYou(e){
-//   e.preventDefault();
-
-//   $(document).append('<div class="hidden">'+
-//     '<span>Thanks for getting in contact, one of our representatives will get back to you soon.In the meantime why don&#8217;t you visit our sister site at:</span>' +
-//     '<button>'+'<a href="http://www.a-e-l.co.uk">www.a-e-l.co.uk</a>'+'</button>'+
-//     '</div>')
-// }
